b8582bd58e6450c1637aa4460fc7a7f833ae2545
zhonghua-wang <zhonghua.wang@outlook.com>
2016-10-28 12:28:35 +0800

    add ng-semantic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import {NgSemanticModule} from 'ng-semantic';
+
 import { AppComponent } from './app.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainMenuComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    NgSemanticModule
   ],
   providers: [],
   bootstrap: [AppComponent]
