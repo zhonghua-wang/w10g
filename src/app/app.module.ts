7f87a95f05601ff670e96d95714fd648b5012493
zhonghua-wang <zhonghua.wang@outlook.com>
2016-11-04 02:50:32 +0800

    change to ng-lightning

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,27 @@
 import './rxjs-extension'
 
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import {MaterialModule} from '@angular/material';
 import {InMemoryWebApiModule} from 'angular-in-memory-web-api';
 
-import { AppComponent } from './app.component';
-import { MainMenuComponent } from './main-menu/main-menu.component';
-import { SideMenuComponent } from './side-menu/side-menu.component';
-import { FooterComponent } from './footer/footer.component';
-import { LearnNg2Component } from './learn-ng2/learn-ng2.component';
-import { AboutComponent } from './about/about.component';
-import { ErrorComponent } from './error/error.component';
-import { HomeComponent } from './home/home.component';
-import { AttributeDirectiveComponent } from './learn-ng2/attribute-directive/attribute-directive.component';
-import { HighlightDirective } from './learn-ng2/attribute-directive/highlight.directive';
-import { MenuComponent } from './menu/menu.component';
+import {AppComponent} from './app.component';
+import {MainMenuComponent} from './main-menu/main-menu.component';
+import {SideMenuComponent} from './side-menu/side-menu.component';
+import {FooterComponent} from './footer/footer.component';
+import {LearnNg2Component} from './learn-ng2/learn-ng2.component';
+import {AboutComponent} from './about/about.component';
+import {ErrorComponent} from './error/error.component';
+import {HomeComponent} from './home/home.component';
+import {AttributeDirectiveComponent} from './learn-ng2/attribute-directive/attribute-directive.component';
+import {HighlightDirective} from './learn-ng2/attribute-directive/highlight.directive';
+import {MenuComponent} from './menu/menu.component';
 import {MenuService} from './menu/menu.service';
 import {InMemoryDataService} from "./in-memory-data.service";
 import {routing} from './app.routing';
+import {NglModule} from 'ng-lightning/ng-lightning';
 
 @NgModule({
   declarations: [
@@ -42,10 +42,14 @@ import {routing} from './app.routing';
     FormsModule,
     HttpModule,
     InMemoryWebApiModule.forRoot(InMemoryDataService),
-    MaterialModule.forRoot(),
-    routing
+    routing,
+    NglModule.forRoot({
+        svgPath: '../node_modules/@salesforce-ux/design-system/assets/icons'
+      }
+    )
   ],
   providers: [MenuService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/side-menu/side-menu.component.ts
+++ src/app/side-menu/side-menu.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Menu, MenuService} from '../menu';
 
 @Component({
   selector: 'app-side-menu',
@@ -7,7 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SideMenuComponent implements OnInit {
 
-  constructor() { }
+  error: any;
+
+  // constructor(
+  //   private menuList: Menu[],
+  //   private menuService: MenuService
+  // ) {};
+
+  // set menuList(mlist) {
+  //   this._menuList = mlist;
+  // }
+  //
+  // get menuList(): Menu[] {
+  //   return this._menuList;
+  // }
+
+  // subMenu(parent: Menu): void{
+  //   this.menuService.getChildren(parent)
+  //     .then(menu => this._menuList = menu)
+  //     .catch(error => this.error = error);
+  // }
 
   ngOnInit() {
   }
